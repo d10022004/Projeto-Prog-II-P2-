3abe7759310905b67a49e135fe84379fa67adc59
d10022004 <david.fidalgo1010@hotmail.com>
2023-06-04 10:07:38 +0100

    Update projetoprog.hs

--- projetoprog.hs
+++ projetoprog.hs
@@ -124,37 +124,73 @@ extrairUCS = map (\(_, _, nome) -> nome)
 ----- PARTE 2 ------------------------------------------------------------------------------------------
 ----- FUNCOES DA ALINEA 1 e 2 --------------------------------------------------------------------------
 
-data Exame = Exame { uc :: String, sala :: String, dia :: String, ano :: Int } deriving (Show, Eq, Ord)
+data Exame = Exame { uc :: String, sala :: String, dia :: String, ano :: Int, alunos :: [String] } deriving (Show, Eq, Ord)
 
-enumerarsalas :: Int -> [String]
-enumerarsalas n = map (\i -> "Sala" ++ show i) [1..n]
+enumerarsalas :: Int -> Int -> [(String, Int)]
+enumerarsalas n lotacao = map (\i -> ("Sala" ++ show i, lotacao)) [1..n]
 
 enumerardias :: Int -> [String]
 enumerardias n = map (\i -> "Dia" ++ show i) [1..n]
 
-gerarEscalonamento :: [(String, Int)] -> [(String, String)] -> [Exame]
-gerarEscalonamento ucs slots = zipWith (\uc slot -> Exame (fst uc) (fst slot) (snd slot) (snd uc)) ucs slots
+gerarEscalonamento :: [(String, Int)] -> [(String, String)] -> [(String, [String])] -> [Exame]
+gerarEscalonamento ucs slots alunos =
+  let exames = zipWith (\uc slot -> Exame (fst uc) (fst slot) (snd slot) (snd uc) []) ucs slots
+  in map (\exame -> exame { sala = fromMaybe "" (lookup (uc exame) slots), alunos = fromMaybe [] (lookup (uc exame) alunos) }) exames
 
 sameDay :: Exame -> Exame -> Bool
-sameDay exame1 exame2 = ano exame1 == ano exame2 && dia exame1 == dia exame2
+sameDay exame1 exame2 = ano exame1 == ano exame2 && dia exame1 == dia exame2 && uc exame1 /= uc exame2
 
 verificarConflitos :: [Exame] -> Bool
 verificarConflitos exames = any (\exame -> length (filter (sameDay exame) exames) > 1) exames
 
-ponto1 :: [(Int, Int, String)] -> Int -> Int -> IO ()
-ponto1 a b c = do
+getAlunosInscritos :: Exame -> [Exame] -> [String]
+getAlunosInscritos exame exames = alunos $ head $ filter ((== uc exame) . uc) exames
+
+calcIncompatibilidadesponto1 :: [Exame] -> [(String, String, Int)]
+calcIncompatibilidadesponto1 exames =
+  let ucs = map uc exames
+      paresUCs = [(uc1, uc2) | uc1 <- ucs, uc2 <- ucs, uc1 /= uc2]
+      incompatibilidades = map (\(uc1, uc2) -> 
+        let exame1 = head $ filter ((== uc1) . uc) exames
+            exame2 = head $ filter ((== uc2) . uc) exames
+            inc = length $ intersect (alunos exame1) (alunos exame2)
+        in (uc1, uc2, inc)) paresUCs
+  in filter (\(_,_,inc) -> inc > 0) incompatibilidades
+
+writeIncompatibilidades :: [Exame] -> IO ()
+writeIncompatibilidades exames = do
+  let incompatibilidades = calcIncompatibilidadesponto1 exames
+  appendFile "escalonamento.txt" "\nIncompatibilidades entre pares de exames:\n"
+  mapM_ (\(uc1, uc2, incompat) -> appendFile "escalonamento.txt" $ "UCs " ++ uc1 ++ " e " ++ uc2 ++ ": " ++ show incompat ++ " alunos inscritos em ambas\n") incompatibilidades
+
+verificarLotacaoSalas :: [(String, Int)] -> Int -> [(String, Int)]
+verificarLotacaoSalas salas lugares =
+  let lotacaoExcedida (sala, alunos) = alunos > lugares
+      salasExcedidas = filter lotacaoExcedida salas
+  in if null salasExcedidas
+       then salas
+       else verificarLotacaoSalas (salas ++ [("Sala" ++ show (length salas + 1), 0)]) lugares
+
+ponto1 :: [(Int, Int, String)] -> [(String, [String])] -> Int -> Int -> Int -> IO ()
+ponto1 a alinsUC b c lugares = do
   let ucseano = extrairUCSeAno a
-  let salas = enumerarsalas b
+  let salas = enumerarsalas b lugares
+  let salasVerificadas = verificarLotacaoSalas salas lugares
   let dias = enumerardias c
-  let slots = [(sala, dia) | sala <- salas, dia <- dias]
-  
+  let slots = [(sala, dia) | (sala, _) <- salasVerificadas, dia <- dias]
+
   if length slots < length ucseano
     then putStrLn "Número insuficiente de salas e/ou dias disponíveis para acomodar todos os exames existentes."
     else do
-        let escalonamento = gerarEscalonamento ucseano slots
+        let escalonamento = gerarEscalonamento ucseano slots alinsUC
         if verificarConflitos escalonamento
           then putStrLn "Há conflitos no escalonamento dos exames."
-          else writeFile "escalonamento.txt" (unlines $ map show escalonamento)
+          else do
+            writeFile "escalonamento.txt" (unlines $ map show escalonamento)
+            writeIncompatibilidades escalonamento
+
+
+
 --------------------------------------------------------------------------------------------------------------------
 ---------Funcoes das alineas 3 e 4----------------------------------------------------------------------------------
 
@@ -162,15 +198,15 @@ type UC = String
 type Aluno = String
 
 -- Função auxiliar para buscar os alunos inscritos em uma UC
-getAlunosInscritos :: UC -> [(UC, [Aluno])] -> [Aluno]
-getAlunosInscritos uc lst = fromMaybe [] (lookup uc lst)
+getAlunosInscritosponto2 :: UC -> [(UC, [Aluno])] -> [Aluno]
+getAlunosInscritosponto2 uc lst = fromMaybe [] (lookup uc lst)
 
 -- Função para calcular as incompatibilidades entre cada par de UCs
 calcIncompatibilidades :: [(UC, [Aluno])] -> [(UC, UC, Int)]
 calcIncompatibilidades inscricoes =
   let ucs = map fst inscricoes
       paresUCs = [(uc1, uc2) | uc1 <- ucs, uc2 <- ucs, uc1 /= uc2]
-      incompatibilidades = map (\(uc1, uc2) -> (uc1, uc2, length (intersect (getAlunosInscritos uc1 inscricoes) (getAlunosInscritos uc2 inscricoes)))) paresUCs
+      incompatibilidades = map (\(uc1, uc2) -> (uc1, uc2, length (intersect (getAlunosInscritosponto2 uc1 inscricoes) (getAlunosInscritosponto2 uc2 inscricoes)))) paresUCs
   in incompatibilidades
 
 -- Exemplo de uso
@@ -193,7 +229,7 @@ recetor d s l = do
     putStrLn ("Indique a opção que prefere\n\t1->Criar ficheiro para Escalonamento\n\t2->Apresentação de incompatibilidades\t")
     op <- opcoes
     if op == 1 
-        then ponto1 cadeiras s d
+        then ponto1 cadeiras inscUCS s d l
         else if op ==2
             then ponto2 inscUCS
             else return()
